9880613b26816fdab12839b5c55e59386a27f61e
MoeinTo <mr.moein.to@gmail.com>
2023-03-12 10:49:49 +0330

    delete defult files

--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 
+import { createApp } from 'vue'
+import Vue from 'vue'
+import App from './App.vue'
+<<<<<<< Updated upstream
+
+=======
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+>>>>>>> Stashed changes
 createApp(App).mount('#app')
